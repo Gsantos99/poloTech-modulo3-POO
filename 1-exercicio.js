6aae5726897ed0ef01958231cc8568993b8d5d6b
Giulyano Santos <giulyanofelipesantos@gmail.com>
2023-01-27 15:03:06 -0300

    Change README.md

--- 1-exercicio.js
+++ 1-exercicio.js
@@ -12,36 +12,36 @@
 
 // Extendendo de Desenvolvedor, crie um FrontEnd e um BackEnd. Cada um deles deve ter métodos próprios e aproveita para adicionar também condicionais, como, por exemplo, o desenvolvedor BackEnd pode ter um método chamado darDesculpa(), onde você gera um número aleatório. Se ele for par, exibe a mensagem "Na minha máquina funciona" e se for impar "Terminei! Falta só testar".
 
-// Suba tudo no git para apresentar na próxima aula :)
+class Animal {
+  nome
+  idade
 
-// Mamifero
-// atributos
-// falar()
+  constructor(nome, idade) {
+    ;(this.nome = nome), (this.idade = idade)
+  }
 
-// Humano filho Mamifero
-// atributos
-// pensar()
-// falar()
+  apresentar() {
+    return `Oi! Eu sou um animal`
+  }
+}
 
-// Desenvolvedor filho Humano
-// atributos
-// codar()
-// falar()
+// const animal1 = new Animal('Elefante', 5)
+// console.log(animal1.apresentar())
 
-// FrontEndDev filho Desenvolvedor
-// atributos
-// codar()
-// centralizarDiv()
-// falar()
+class Mamifero extends Animal {
+  nome
+  idade
+  temPelo 
 
-// BackEndDev filho Desenvolvedor
-// atributos
-// codar()
-// darDesculpa(); {
-//     geraUmNumeroAleatorio
-//         par
-//          "na minha funciona"
-//         impar
-//          "terminei, falta so testar"
-// }
-// falar()
+  constructor(nome, idade, temPelo) {
+    super(nome, idade)
+    ;(this.nome = nome), (this.idade = idade), (this.temPelo = temPelo)
+  }
+
+  apresentar() {
+    return `Oi! Eu sou um(a) ${this.nome} e sou mamifero`
+  }
+}
+
+const mamifero1 = new Mamifero('lobo', 5, true)
+console.log(mamifero1.apresentar())
